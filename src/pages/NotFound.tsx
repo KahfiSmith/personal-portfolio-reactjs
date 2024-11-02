0a46b311b4ce0af4ee5d25bbafc9c6a05a7ba3c5
KahfiSmith <alkahfii2018@gmail.com>
2024-11-02 21:08:42 +0700

    fix(background): page not found

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -8,14 +8,18 @@ export default function NotFound() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-[100px] font-bold text-slate-600">404</h1>
-      <p className="text-slate-600 mb-6">
+    <div className="relative flex flex-col items-center justify-center h-screen bg-slate-900 overflow-hidden">
+      <div className="custom-gradient-background"></div>
+
+      <h1 className="text-[100px] font-bold text-slate-200 -tracking-tighter relative z-10">
+        404
+      </h1>
+      <p className="text-slate-200 mb-6 relative z-10">
         Oops! The page you're looking for doesn't exist.
       </p>
       <button
         onClick={handleGoBack}
-        className="px-4 py-2 bg-[#7939ef] text-white rounded font-medium transition-all"
+        className="px-10 py-2 bg-gradient-to-r from-[#273751] to-[#182237] text-white rounded font-medium transition-all hover:shadow-lg hover:shadow-[#2b3c57]/70 shadow-lg shadow-[#2b3c57]/50 border-1 border-slate-800"
       >
         Go Back
       </button>
