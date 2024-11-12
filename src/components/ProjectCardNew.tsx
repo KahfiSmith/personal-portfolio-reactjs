3a19b9cdd5d7a496067dc6825bf850d855d54165
KahfiSmith <alkahfii2018@gmail.com>
2024-11-12 22:11:06 +0700

    fix(projects): fix blur and color

--- src/components/ProjectCardNew.tsx
+++ src/components/ProjectCardNew.tsx
@@ -11,10 +11,10 @@ export default function ProjectCardNew({
       <img
         src={imgSrc}
         alt={title}
-        className="w-full object-cover transition-transform duration-500 ease-in-out group-hover:scale-150"
+        className="w-full object-cover transition-transform duration-500 ease-in-out group-hover:scale-150 group-hover:rotate-6"
       />
       <div className="absolute inset-0 bg-black/50 hover:bg-black/20 flex flex-col justify-between ">
-        <div className="bg-transparent backdrop-blur-sm p-3 rounded-t-lg">
+        <div className="bg-transparent backdrop-blur-sm p-3 rounded-t-lg group-hover:backdrop-blur-0">
           <h2 className="text-lg font-semibold">{title}</h2>
           <p className="text-sm">{desc}</p>
         </div>
