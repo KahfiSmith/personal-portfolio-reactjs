14237888ffc78c85ca731d884b1209caf8977a6c
KahfiSmith <alkahfii2018@gmail.com>
2024-11-14 12:46:46 +0700

    fix(about): padding layout

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,7 @@ import { dataExperience } from "@/lib/utils/dataExprerience";
 export default function About() {
   return (
     <section className="w-full flex justify-center pt-20 min-h-screen" id="about">
-      <div className="w-full md:flex md:flex-col items-center px-16 max-w-7xl">
+      <div className="w-full md:flex md:flex-col items-center px-4 max-w-7xl">
         <div className="flex flex-col justify-start text-[20px] md:text-2xl lg:text-3xl font-medium mb-6">
           <span className="md:mb-5 mb-1 text-customText">
             I'm a Frontend Web Developer and UI/UX Designer
