cd62b2f1a41ca042730fb83747e45f18dbc7bede
KahfiSmith <alkahfii2018@gmail.com>
2024-11-25 07:51:24 +0700

    fix(hobby): text description

--- src/lib/utils/dataHobby.tsx
+++ src/lib/utils/dataHobby.tsx
@@ -3,38 +3,38 @@ import { ProjectsItemProps } from "@/types";
 export const dataHobby: ProjectsItemProps[] = [
   {
     imgSrc: "/images/gradient.png",
-    title: "Graduient",
-    desc: "A complete e-commerce application built with React and Node.js",
+    title: "Gradient",
+    desc: "Gradient design graphics with futuristic color combinations",
     type: "design",
   },
   {
     imgSrc: "/images/nobara.png",
     title: "Nobara",
-    desc: "A complete e-commerce application built with React and Node.js",
+    desc: "Nobara Kugisaki from Jujutsu Kaisen with a retro design style",
     type: "design",
   },
   {
     imgSrc: "/images/pathetic.png",
     title: "Patethic",
-    desc: "A complete e-commerce application built with React and Node.js",
+    desc: "Yuichi Katagiri from the Tomodachi Game with a black and white design style",
     type: "design",
   },
   {
     imgSrc: "/images/bocchi.jpg",
     title: "Bocchi",
-    desc: "A complete e-commerce application built with React and Node.js",
+    desc: "Bocchi from Bocchi the Rock with a grainy texture design style",
     type: "design",
   },
   {
     imgSrc: "/images/shiro.jpg",
     title: "Shiro",
-    desc: "A complete e-commerce application built with React and Node.js",
+    desc: "Shiro from No Game No Life with a milky ways design style",
     type: "design",
   },
   {
     imgSrc: "/images/makima.jpg",
     title: "Makima",
-    desc: "A complete e-commerce application built with React and Node.js",
+    desc: "Makima from Chainsaw Man with a retro design style",
     type: "design",
   },
 ];
